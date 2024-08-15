4d95c2338a65145c1671e5055bd497b458bf1663
uie41442(Zheng Zhicheng) <zhicheng.zheng@continental-corporation.com>
2024-08-15 11:17:13 +0800

    Config adaptaion

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,17 +11,17 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteUrl: 'https://sunroof.zhengzhicheng.com/run',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  description: 'Zheng Zhicheng Running History',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://sunroof.zhengzhicheng.com',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://github.com/zhengfran',
     },
   ],
 };
